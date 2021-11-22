fb97108b656cf23ad5fc6ff1814e02cce83fa915
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-11-21 21:39:31 -0500

    server js updates

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(logger('dev'));
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
